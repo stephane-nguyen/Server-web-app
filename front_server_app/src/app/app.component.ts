83528aec71d0082ca6d479c02f854139eeeaa9de
Stéphane <stepdh01@gmail.com>
2022-09-18 18:30:46 +0200

    ping server to do

--- front_server_app/src/app/app.component.ts
+++ front_server_app/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  startWith,
+} from 'rxjs';
+import { DataState } from './enum/data.state.enum';
+import { Status } from './enum/status.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'front_server_app';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+  //readonly: dont need to use it in this class, we can use the var in component.html
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  filterStatus$ = this.filterSubject.asObservable();
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED_STATE, appData: response };
+      }),
+      //the return refers to http request => take some time => meanwhile display LOADING_STATE
+      //appdata: null => remove it, optional
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        //error: error <=> error in js
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
 }
